261bbc113de1222ef80b8076a274182acf8600c4
ossi keva <143818496+osjomike@users.noreply.github.com>
2024-11-06 15:22:49 +0200

    Add files via upload

---
+++ 1. Scripts/1.1 Isotope Biplot.R
@@ -0,0 +1,157 @@
+rm(list = ls()) # clear R's clobal envirnoment memory
+
+## Author: Ossi Keva
+## Date 03.06.2024
+## Project: Roger I Jones -- Allocarb, Antti Eloranta -- FreshRestore
+
+## Short description: With this script we are plotting the "isotope biplots". Article Fig 2.
+
+## Some packages to download
+library(dplyr) ## pipe operatore
+library(reshape2) ## melt and dcast
+library(ggplot2) ## plotting functions
+library(ggnewscale) ## some colour layer functions
+library(cowplot) ## plotting functions
+library(gridExtra) ## plotting functions
+library(grid)
+
+## Setting working directory
+setwd("D:\\Keva et al. NC 2024") ## Set correct wd here
+orgfolder<-getwd()
+## Patch for data
+Data_dir<-file.path(orgfolder,"2. Data")
+
+## downloading some data sets
+consumers<-read.csv2(file = paste0(Data_dir, "/Consumerdata_w_0.23newTP_2.1.csv"), sep = ",", dec=".")
+sources<-read.csv2(file = paste0(Data_dir, "/sourcedata_frac.csv"), sep = ",", dec=".")
+Lake_PCA<-read.csv2(file = paste0(Data_dir, "/lake_PCA_scores.csv"), sep = ",", dec=".", fileEncoding = "latin1")
+water<-read.csv2(file = paste0(Data_dir, "/Lake water d2H and source values.csv"), sep = ";", dec=".")  
+
+# Checking column names
+#colnames(consumers)
+#colnames(sources)
+#colnames(Lake_PCA)
+#colnames(water)
+## Modifying column names to match
+colnames(sources)[1]<-"Source"
+colnames(Lake_PCA)[2]<-"Lake"
+## Changing the Scandic letters from lake names to ä->a, Ä->A, ö->o
+Lake_PCA$Lake<-chartr("äÄö", "aAo", Lake_PCA$Lake)
+water$Lake<-chartr("äÄö", "aAo", water$Lake)
+
+## Renaming some variables
+water$Source<-"water"
+water$Meand2H<-water$"d2H_water_center"
+water$SDd2H<-0 ## Setting water d2H SD to 0
+
+## Subsetting water dataframe the files has other lakes as well.
+lakes<-unique(Lake_PCA$Lake)
+water_sub<-water[which(water$Lake %in% lakes),]
+
+#colnames(sources)
+source_water<-dplyr::bind_rows(sources[c("Lake", "Source","Meand2H", "SDd2H")], 
+                               water_sub[c("Lake", "Source","Meand2H", "SDd2H")])
+
+source_water_PC<-merge(x=source_water, y=Lake_PCA, by="Lake")
+
+
+### OK THEN WE NEED TO DO TL LABELS
+labels_n<-consumers %>%
+  group_by(species) %>%
+  summarise(n=sum(!is.na(d2H)), n_lakes=length(unique(Lake)), TL_=mean(TL, na.rm=TRUE), TL_SD=sd(TL, na.rm=TRUE))
+
+### Creating a figure list and doing the plots
+fig_list<-list()
+for (i in unique(consumers$species)){
+  fig_list[[i]]<-ggplot(consumers[which(consumers$species ==i),], aes(x=PC1, y=d2H))+
+    geom_point(data=source_water_PC, aes(x=PC1, y=Meand2H, colour=Source), size=1.2, shape=4, inherit.aes = FALSE)+
+    geom_linerange(data=source_water_PC, aes(x=PC1, ymin=Meand2H-SDd2H, ymax=Meand2H+SDd2H, group=Source, colour=Source), inherit.aes =FALSE) +
+    scale_colour_manual("Key for colours", values=c("purple","brown","lightblue","black"),
+                        limits=c("water","Ter","Aq","consumer"), ## one can modify the order of the legend with this
+                        labels=c("Water","Allochtonous","Autochtonous",  "Consumer"))+
+
+    new_scale_color() + ## This is from ggnewscale, one can add several colour palettes to different datasets using this, but here we are using only one colour still
+    geom_point(aes(y=d2H, x=PC1),shape=10, colour="black")+
+    geom_smooth(aes(y=d2H, x=PC1), show.legend=FALSE, colour="black")+
+    #geom_smooth(method="lm",colour="black",show.legend = FALSE)+
+    #scale_colour_manual(values = my_colours)+
+    #scale_shape_manual(values = my_shapes)+
+    geom_text(data=labels_n[which(labels_n$species ==i),], x=4.5,y=-250, hjust=1,vjust=0, size=2,
+              aes(label=paste0("n=",n,"; lakes=", n_lakes, 
+                               "; TL=", sprintf(TL_, fmt = '%#.1f'), "±", sprintf(TL_SD, fmt = '%#.1f'))))+
+    
+    labs(y=expression(paste(delta^{2}, "H (\u2030)"["cor, w=0.23"])),
+         x=c("Forestry, DOC <------ PC1 ------> Agriculture, nutrients"),
+         title = i)+
+    coord_cartesian(ylim=c(-250,-50))+ #, xlim=c(6,-4))+
+    theme_classic()+
+    theme(legend.position="none",
+          axis.text.x=element_blank(), #remove x axis labels
+          #axis.ticks.x=element_blank(), #remove x axis ticks
+          axis.text.y=element_blank(),  #remove y axis labels
+          #axis.ticks.y=element_blank(),  #remove y axis ticks
+          axis.title = element_blank(),
+          plot.title=element_text(size=10),
+          plot.background = element_blank(), panel.background = element_rect(fill="grey100", colour = "grey100"))
+}
+## fig_list[[1]] just checking random figure from list, that the loop worked
+
+### Getting the legend
+legend<-get_legend(fig_list[[1]]+theme(legend.position="right",
+                                       legend.box = "horizontal", 
+                                       legend.background = element_rect(linetype = 2, size = 0.5, colour = 1),
+                                       legend.title=element_text(),
+                                       legend.justification = NULL,
+                                       legend.margin=margin(c(5,5,5,5)),
+                                       plot.margin=unit(c(0,0,0,0), units="line")))
+legend1<- plot_grid(legend) # Checking the legend
+
+fig_order<-c("Bulk_BMI_littoral", "Perch_large", "Pike", 
+             "Bulk_ZPL", "Asellus_littoral", "Perch_small", "Smelt",
+             "Chaoborus", "Chironomid_littoral", "Vendace", "Bleak", 
+             "Cladocera", "Bulk_BMI_profundal", "Perch_medium", "Roach_small",
+             "Copepods", "Chironomid_Profundal", "Ruffe", "Roach_large")
+
+new_figlist1<-list()
+new_figlist1<-fig_list[fig_order]
+new_figlist1<- append(new_figlist1,list(legend1), after = 0)
+comb_fig<-cowplot::plot_grid(plotlist = new_figlist1, ncol=4)
+
+### Finally doing the plot and adding axis legends. 
+### Add the axis labels in incscape or similar, check labels e.g. here fig_list[[1]]
+
+pdf(file="4. Figures/isotope biplots_v2.pdf", 
+    height = 7, width = 6.5)
+grid.arrange(comb_fig, left = textGrob(expression(paste(delta^{2}, "H (\u2030)"["cor, w=0.23"])),rot=90, gp = gpar(fontsize = 11)), 
+             bottom = textGrob("Forestry, DOC<--  PC1 axis  --> Agriculture, nutrients",gp = gpar(fontsize = 11)))
+dev.off()
+
+### Checking the samples outside the mixing envelope ####
+mean<-dcast(sources, Lake~Source, value.var=c("Meand2H"))
+sd<-dcast(sources, Lake~Source, value.var="SDd2H")
+
+colnames(mean)<-c("Lake", "Aq_mean", "Ter_mean")
+colnames(sd)<-c("Lake", "Aq_sd", "Ter_sd")
+ss<-merge(mean, sd, by="Lake")
+consumers_sources<-merge(consumers, ss, by="Lake")
+
+## Samples outside source envelope source+-2SD
+consumers_sources$diff_aq<-consumers_sources$d2H-(consumers_sources$Aq_mean-2*consumers_sources$Aq_sd)
+consumers_sources$diff_Ter<-consumers_sources$d2H-(consumers_sources$Ter_mean+2*consumers_sources$Ter_sd)
+
+errors<-consumers_sources %>%
+  group_by(Lake, species) %>%
+  #summarise_at(vars(sd), funs(grand_SD=grand.mean(.,N=n)))
+  summarise(Diff_TER=sum(diff_Ter>0), Diff_aq=sum(diff_aq<0))
+
+#If you are interested which consumers in which lakes were outside the envelope
+x<-dcast(errors, Lake~species, value.var = "Diff_aq")
+y<-dcast(errors, Lake~species, value.var = "Diff_TER")
+
+### Checking how many samples were outside the envelope their percentage from total number of consumers
+sum(errors$Diff_TER)+sum(errors$Diff_aq)
+(sum(errors$Diff_TER)+sum(errors$Diff_aq))/length(consumers$d2H)
+
+#########################
+### END OF THE SCRIPT ###
+#########################
---
+++ 1. Scripts/4.1 model comparisons test.R
@@ -0,0 +1,102 @@
+rm(list=ls())
+
+## Author: Ossi Keva
+## Date 6.11.2024
+## Project: Roger I Jones -- Allocarb, Antti Eloranta -- FreshRestore
+
+## Short description: with this script we are comparing MixSIAR models with different covariates
+## Suppelementary table 8
+
+# Some packages to download
+library(MixSIAR)
+library(ggplot2)
+
+## Selecting working directory
+setwd("D:\\Keva et al. NC 2024")
+orgfolder<-getwd()
+
+folders<-c("very long_Mod with lake+species_rand+PC1 w_0.23_randSlope",
+           "very long_Mod with lake+species_rand+For_area w_0.23_randSlope",
+           "very long_Mod with lake+species_rand+POM_NC_ratio w_0.23_randSlope",
+           "long_Mod with lake+species w_0.23",
+           "long_Mod with lake w_0.23",
+           "long_empty")
+
+jags.objects<-c("mixSIAR_model_Mod with lake+species_rand+PC1 w_0.23_randSlope.rds",
+                "mixSIAR_model_Mod with lake+species_rand+For_area w_0.23_randSlope.rds",
+                "mixSIAR_model_Mod with lake+species_rand+POM_NC_ratio w_0.23_randSlope.rds",
+                "mixSIAR_model_Mod with lake+species w_0.23.rds",
+                "mixSIAR_model_Mod with lake w_0.23.rds",
+                "mixSIAR_model_empty.rds" )
+
+model_list<-list()
+
+## Upload jags objects to model_list
+for(i in 1:length(jags.objects)){
+  setwd(file.path(orgfolder, "3. MixSIAR Models", folders[i]))
+  model_list[[i]]<-readRDS(file=jags.objects[i])
+  setwd(orgfolder)
+}
+
+## Give model names
+names(model_list)<-c("PC1+(1+PC1|species)+(1|species)+(1|Lake)",
+                     "For%+(1+For%|species)+(1|species)+(1|Lake)",
+                     "(POM_N:C)+(1+POM_N:C|species)+(1|species)+(1|Lake)",
+                     "(1|species)+(1|Lake)",
+                     "(1|species)",
+                     "1")
+
+# run the model comparison test
+compare_models(model_list, loo=TRUE)
+#warnings()
+#getAnywhere(compare_models)
+
+
+### OK, now lets observe the covariate slope distribution in ILR space and how it affects the mixtures 
+
+# making toy data first column is aq and second ter, This is proportional data 
+# mixSIAR load_source_data() -function organises the sources alphabetically, 
+# so this toy data emulates the situation we are presenting in the paper
+test_data<-matrix(c(c(0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 0.9, 0.99, 0.999),
+                    c(0.999, 0.99, 0.9, 0.7, 0.5, 0.3, 0.1, 0.01, 0.001)), ncol = 2)
+## Making ILR conversion of the the toy data to check how does the transformation impact the proportions
+ILR_2<-c()
+for ( i in 1:nrow(test_data)){
+  ILR_2[i]<-sqrt(1/(1+1))*log(test_data[i,1]/test_data[i,2])
+}
+
+## We can observe that higher ILR scores indicate higher contribution of test_data[1] values
+## In our case (manuscript) these higher ILR scores indicate higher aquatic dietary proportion in consumers
+## and lower terrestrial contribution. In the other hand low ILR scores indicate high terrestrial contribution. 
+
+## Then lets check what is the slope distribution in our model
+PC1<-model_list[[1]]
+CIs<-quantile(PC1[["BUGSoutput"]][["sims.list"]][["ilr.cont1"]], probs = c(0.025, 0.5, 0.975))
+scaleFUN <- function(x) sprintf("%.3f", x)
+
+library(ggplot2)
+dat<-data.frame(slope=PC1[["BUGSoutput"]][["sims.list"]][["ilr.cont1"]])
+distribution<-ggplot(dat, aes(x=slope))+ 
+  geom_histogram(aes(y=..density..), binwidth = 0.01, colour="black", fill="white")+
+  geom_density(fill="#FF6666", alpha=0.5)+ 
+  geom_vline(xintercept = 0, linetype="dashed", colour="grey")+
+  coord_cartesian(xlim = c(-1,1))+
+  annotate(geom="text", label=paste0("median (95% CI) = ", scaleFUN(CIs[2]), 
+                                      " (", scaleFUN(CIs[1]), "--", scaleFUN(CIs[3]), ")"),
+           x=0.5, y=4.5, size=4)+
+  labs(y="Scaled density", x="Linear coefficient with PC1 in ILR space")+
+  theme_classic()
+
+#png(file = "4. Figures/Linear coefficient with PC1 in ILR space.png",
+#    width = 7, height= 3, res=300, units="in")
+#distribution
+#dev.off()
+
+### Now when comparing the ILR conversion and the slope distribution 
+### we can conclude that increasing PCA increases aquatic source contribution in the consumer dietary 
+### as higher ILR scores indicate higher aquatic contribution 
+### and because of the coeffiecient have a positive slope distribution
+
+#########################
+### END OF THE SCRIPT ###
+#########################
